682f41b39408a7209d4e33f6de13d38dc2b248d5
Bayram ARSLAN <by.ram1789@gmail.com>
2023-03-16 18:11:57 +0300

    Dizide yeralan sayıların harmonik ortalaması

---
+++ harmonicMean/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double[] list = {1,2,3,4,5,6};
+        double mean = 0;
+        for (int i = 0; i < list.length; i++){
+            mean += 1/(list[i]);
+        }
+        double harmonicMean = list.length / mean;
+        System.out.println(harmonicMean);
+    }
+}
